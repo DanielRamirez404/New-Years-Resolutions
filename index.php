1cf816d5c28ce45581bbdf41987b2dd35cad635e
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-01-14 02:22:13 -0400

    Add index.php

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
-	echo "Hello world!"
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $server = "localhost";
+        $db = "resolutions";
+
+        try 
+        {
+            $conn = new mysqli($server, $username, $password, $db); 
+        }
+        catch(Exception $exception)
+        {
+            $error = "An error occured: " . $exception->getMessage();
+        }
+    }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+    <h2>Login</h2>
+
+    <?php if (isset($error)) : ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
