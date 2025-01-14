cacf057ecade13ab1e1189aeb3d60ec0c000f344
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-01-14 03:03:10 -0400

    styled index.php using Bootstrap 5

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
 
         try 
         {
-            $conn = new mysqli($server, $username, $password, $db); 
+            $connection = new mysqli($server, $username, $password, $db);
+            $_SESSION["connection"] = $connection;
+            header("Location: menu.php");
+            exit();
         }
         catch(Exception $exception)
         {
@@ -22,7 +25,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+            font-family: sans-serif;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            min-height: 100vh;
+            padding: 25px;
+            background-color: #f0f0f0;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -31,15 +49,17 @@
     <?php if (isset($error)) : ?>
         <p style="color: red;"><?php echo $error; ?></p>
     <?php endif; ?>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-
-        <button type="submit">Login</button>
+    
+    <form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+            <button type="submit" class="btn btn-primary">Login</button>
     </form>
 
 </body>
